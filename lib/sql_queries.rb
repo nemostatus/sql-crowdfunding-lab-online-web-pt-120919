47456d9dceea866506ebd3edd55b842602eed582
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-27 11:37:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(amount) projects.funding_goal FROM projects pledgesINNER JOIN pledges WHERE pledges.project_id = projects.id GROUP BY projects.title HAVING projects.id = pledges.project_id ORDER BY SUM(amount), projects.funding_goal > -1 DESC LIMIT 2;"
+  "SELECT projects.title, SUM(amount) projects.funding_goal FROM projects, pledges INNER JOIN  WHERE pledges.project_id = projects.id GROUP BY projects.title HAVING projects.id = pledges.project_id ORDER BY SUM(amount), projects.funding_goal > -1 DESC LIMIT 2;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
